305b7ce455e2b24a1a2989a82f107e39aaeddff8
Kad Aliu <88911431+kad1kad@users.noreply.github.com>
2024-10-19 21:57:24 +0100

    Add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,9 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Abernathy'
+
+return config
