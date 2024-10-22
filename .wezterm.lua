05be94784efa83defe34b3ac192b8997c271a554
Kad Aliu <88911431+kad1kad@users.noreply.github.com>
2024-10-22 19:03:48 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,12 +3,19 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+config.window_close_confirmation = "NeverPrompt"
 
 config.color_scheme = 'Atom'
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 20
+-- config.window_background_opacity = 0.9
+-- config.macos_window_background_blur = 10
+-- config.colors = {
+--   background = '#222a36'
+-- }
 
 config.use_fancy_tab_bar = false
+
+config.initial_cols = 100
+config.initial_rows = 150
 
 -- Disable title bar but enable resizable border
 config.window_decorations = "RESIZE"
@@ -17,11 +24,10 @@ config.font_size = 16
 
 -- Disable treating Alt as a modifier for commands
 -- and let it work as a normal key
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = true
+-- config.send_composed_key_when_left_alt_is_pressed = true
+-- config.send_composed_key_when_right_alt_is_pressed = true
 
 config.hide_tab_bar_if_only_one_tab = true
-config.window_close_confirmation = "NeverPrompt"
 
 local act = wezterm.action
 
