7547434035d2daa3783a59f51c8e9c70a1c2080f
Kad Aliu <88911431+kad1kad@users.noreply.github.com>
2024-10-22 19:04:59 +0100

    Update config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,20 +1,21 @@
-
 lvim.colorscheme = "nordic"
--- overrides nvim bg color theme with wezterm bg color
 lvim.transparent_window = true
 
 -- install plugins
 lvim.plugins = {
-  "AlexvZyl/nordic.nvim", 
+  "AlexvZyl/nordic.nvim",
   {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-      -- add any options here
     },
     dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- "rcarriga/nvim-notify",
     }
   },
   {
@@ -27,7 +28,53 @@ lvim.plugins = {
       })
     end,
   },
+  {
+  "f-person/git-blame.nvim",
+  event = "BufRead",
+  config = function()
+    vim.cmd "highlight default link gitblame SpecialComment"
+    require("gitblame").setup { enabled = true }
+  end,
+},
+{
+  "windwp/nvim-ts-autotag",
+  config = function()
+    require("nvim-ts-autotag").setup()
+  end,
+},
+{
+  "mrjones2014/nvim-ts-rainbow",
+},
+{
+  "karb94/neoscroll.nvim",
+  event = "WinScrolled",
+  config = function()
+  require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+        '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = true,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = false, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,        -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,              -- Function to run after the scrolling animation ends
+        })
+  end
+},
+{
+  "andymass/vim-matchup",
+  event = "CursorMoved",
+  config = function()
+    vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  end,
+},
 }
+
+-- enable treesitter integration for matchup
+lvim.builtin.treesitter.matchup.enable = true
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
@@ -35,6 +82,7 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = nil })
 end
 
+vim.opt.relativenumber = true
 vim.opt.wrap = true
 
 -- automatically install python syntax highlighting
@@ -58,7 +106,7 @@ formatters.setup { { name = "black" },
     -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
     args = { "--print-width", "100" },
     ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
-    filetypes = { "typescript", "typescriptreact", "scss", "css", "heex", "elixir", "html"},
+    filetypes = { "typescript", "typescriptreact", "scss", "css" },
   },
   {
     name = "djlint",
@@ -97,6 +145,10 @@ lvim.keys.normal_mode["-"] = ":split<CR>"
 lvim.keys.normal_mode["<TAB>"] = ":bnext<CR>"
 lvim.keys.normal_mode["<leader>E"] = ":NvimTreeFocus<CR>"
 lvim.keys.normal_mode["<CR>"] = ":noh<CR><CR>"
+lvim.keys.normal_mode["<leader>o"] = ":let @+ = expand('%')<CR>"
+lvim.keys.normal_mode["<leader>i"] = ":let @+ = 'npx stylelint ' . expand('%') . ' --fix'<CR>"
+lvim.keys.normal_mode["<A-3>"] = nil
+lvim.keys.insert_mode["<A-3>"] = nil
 lvim.keys.normal_mode["c"] = '"_c'
 lvim.keys.normal_mode["C"] = '"_C'
 
@@ -109,3 +161,20 @@ require 'nvim-treesitter.configs'.setup {
   }
 }
 
+lvim.builtin.alpha.dashboard.section.header.val = {
+[[]],
+[[ ██ ▄█▀▄▄▄      ▓█████▄      ██▒   █▓ ██▓ ███▄ ▄███▓ ]],
+[[ ██▄█▒▒████▄     ▒██▀ ██▌   ▓██░   █▒▓██▒▓██▒▀█▀ ██▒ ]],
+[[ ▓███▄░▒██  ▀█▄  ░██   █▌    ▓██  █▒░▒██▒▓██    ▓██░ ]],
+[[ ▓██ █▄░██▄▄▄▄██ ░▓█▄   ▌     ▒██ █░░░██░▒██    ▒██  ]],
+[[ ▒██▒ █▄▓█   ▓██▒░▒████▓       ▒▀█░  ░██░▒██▒   ░██▒ ]],
+[[ ▒ ▒▒ ▓▒▒▒   ▓▒█░ ▒▒▓  ▒       ░ ▐░  ░▓  ░ ▒░   ░  ░ ]],
+[[ ░ ░▒ ▒░ ▒   ▒▒ ░ ░ ▒  ▒       ░ ░░   ▒ ░░  ░      ░ ]],
+[[ ░ ░░ ░  ░   ▒    ░ ░  ░         ░░   ▒ ░░      ░    ]],
+[[ ░  ░        ░  ░   ░             ░   ░         ░    ]],
+[[                  ░              ░                   ]],
+[[]],
+
+}
+-- turn off line wrap
+-- vim.opt.wrap = false
