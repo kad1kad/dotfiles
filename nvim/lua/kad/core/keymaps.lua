0f6dad38836bbf411c8647f7ffe2805faa9fbf7a
Kad Aliu <88911431+kad1kad@users.noreply.github.com>
2024-12-14 19:12:21 +0000

    Update config

--- nvim/lua/kad/core/keymaps.lua
+++ nvim/lua/kad/core/keymaps.lua
@@ -13,26 +13,33 @@ keymap("v", "c", '"_c', opts)
 keymap("v", "C", '"_C', opts)
 
 -- Yank entire file to system clipboard
-keymap("n", "<leader>y", ":%y+<CR>", { noremap = true, silent = false })
+keymap("n", "<leader>y", ":%y+<CR>", opts)
 
 -- Move line up
-keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+keymap("n", "<C-k>", ":m .-2<CR>==", opts)
 -- Move line down
-keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<C-j>", ":m .+1<CR>==", opts)
 
 -- Visual mode: Move selected lines up
 keymap("v", "<C-k>", ":m '<-2<CR>gv=gv", opts)
 -- Visual mode: Move selected lines down
 keymap("v", "<C-j>", ":m '>+1<CR>gv=gv", opts)
 
--- Change inner word (shortcut for `ciw`)
+-- Change inner word
 keymap("n", "<leader>s", "ciw", opts)
 
--- Select all (like `ggVG`)
+-- Change inside "
+keymap("n", "<leader>\"", "ci\"", opts)
+
+-- Change inside '
+keymap("n", "<leader>\'", "ci\'", opts)
+
+-- Select entire document
 keymap("n", "<leader>a", "ggVG", opts)
 
--- Go back a word (shortcut for `b`)
+-- Go back a word with backspace
 keymap("n", "<BS>", "b", opts)
+keymap("v", "<BS>", "b", opts)
 
 -- Write file with leader w
 keymap("n", "<leader>w", ":w<CR>", opts)
@@ -52,13 +59,11 @@ keymap('n', '<leader>gb', ':Git blame<CR>', opts)
 keymap('n', '<leader>gi', ':Git<CR>', opts)
 
 -- Resize panes with Alt + hjkl
-vim.api.nvim_set_keymap("n", "<M-h>", ":vertical resize -2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<M-l>", ":vertical resize +2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<M-k>", ":resize -2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<M-j>", ":resize +2<CR>", opts)
+keymap("n", "<M-h>", ":vertical resize -2<CR>", opts)
+keymap("n", "<M-l>", ":vertical resize +2<CR>", opts)
+keymap("n", "<M-k>", ":resize -2<CR>", opts)
+keymap("n", "<M-j>", ":resize +2<CR>", opts)
 
 vim.keymap.set("n", "<leader>dh", function()
   vim.diagnostic.open_float(nil, { focus = false })
 end, { desc = "Show diagnostics on hover" })
-
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
